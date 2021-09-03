93dc03612837fd33b7432e977254160d6ae82332
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-04 02:07:41 +0300

    added an array for male and female names on js file

--- js/main.js
+++ js/main.js
@@ -6,50 +6,50 @@ function akanName() {
 
     let gender = document.getElementById("gender").value;
 
-    // let days = [
-    //   "Sunday",
-    //   "Monday",
-    //   "Tuesday",
-    //   "Wednesday",
-    //   "Thursday",
-    //   "Friday",
-    //   "Saturday",
-    // ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-    // let maleNames = [
-    //   "Kwase",
-    //   "Kwadwo",
-    //   "Kwabena",
-    //   "Kwaku",
-    //   "Yaw",
-    //   "Kofe",
-    //   "Kwame",
-    // ];
+    let maleNames = [
+      "Kwase",
+      "Kwadwo",
+      "Kwabena",
+      "Kwaku",
+      "Yaw",
+      "Kofe",
+      "Kwame",
+    ];
 
-    // let femaleNames = [
-    //   "Akosua",
-    //   "Adwoa",
-    //   "Abenaa",
-    //   "Akua",
-    //   "Yaa",
-    //   "Afua",
-    //   "Ama",
-    // ];
+    let femaleNames = [
+      "Akosua",
+      "Adwoa",
+      "Abenaa",
+      "Akua",
+      "Yaa",
+      "Afua",
+      "Ama",
+    ];
 
-    // let dateString = date + "#" + month + "#" + year;
-    // let birthDate = new Date(dateString);
-    // let currentDate = birthDate.getDay();
-    // if (date <= 0 || date > 31) {
-    //   alert("Please Enter correct date");
-    // } else if (month < 1 || month > 12 || (month == 2 && day > 29)) {
-    //   alert("please enter valid month");
-    // } else if (year <= 0 || year > 2050) {
-    //   alert("Please Enter a Valid Year");
-    // } else if (gender.length === 0) {
-    //   alert("Please Select your Gender");
-    // }
-    // let currentDayName = days[currentDate];
-    // switch (gender) {
+    let dateString = date + "#" + month + "#" + year;
+    let birthDate = new Date(dateString);
+    let currentDate = birthDate.getDay();
+    if (date <= 0 || date > 31) {
+      alert("Please Enter correct date");
+    } else if (month < 1 || month > 12 || (month == 2 && day > 29)) {
+      alert("please enter valid month");
+    } else if (year <= 0 || year > 2050) {
+      alert("Please Enter a Valid Year");
+    } else if (gender.length === 0) {
+      alert("Please Select your Gender");
+    }
+    let currentDayName = days[currentDate];
+    switch (gender) {
       // if male gender is submit the following line of code will be executed.
 
     //   case "male":
