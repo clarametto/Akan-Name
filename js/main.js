b285ac245fffd36d0b9b28d0e207f1d8aec23a69
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-03 23:22:18 +0300

    create a js function on js file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function akanName(){
+    let date = document.getElementById("date").value;
+    let month = document.getElementById("month").value;
+    let year = document.getElementById("year").value;
+
+    let gender = document.getElementById("gender").value;
+
+
+}
