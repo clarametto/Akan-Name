d49836da80b206628f2c5f902311c6c80e6bba1b
clara chepngeno metto <clara.metto@student.moringaschool.com>
2021-09-05 14:26:44 +0300

    Updated script link on name.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function akan() {
+function akanName() {
   function akanNameFunction() {
     let date = document.getElementById("date").value;
     let month = document.getElementById("month").value;
